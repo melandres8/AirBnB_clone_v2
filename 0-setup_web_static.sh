cedcff4663a80f77b34c6510e3c61d7b961a4fab
melandres8 <melanmosta@gmail.com>
2020-08-17 22:36:26 +0000

    task#0: adjustments

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,5 +14,5 @@ sudo echo "<html>
 </html> " | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server/a \\tlocation /hbnb_static/ {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo sed -i '/listen 80 default_server/a \\tlocation /hbnb_static {\n\t\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo /etc/init.d/nginx restart

ac3bd05f032806c05ac5f458e5628c4cb028f105
melandres8 <melanmosta@gmail.com>
2020-08-17 22:07:19 +0000

    task#0: adjustments

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,7 +3,6 @@
 
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
@@ -19,4 +18,3 @@ sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a \\tlocation /hbnb_static/ {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
-exit(0)

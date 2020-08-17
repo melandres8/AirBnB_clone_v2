4188603182dffc3c8c972e35e993515e551b3899
melandres8 <melanmosta@gmail.com>
2020-08-17 22:05:29 +0000

    task#0: adjustments

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,17 +7,16 @@ sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-sudo echo "
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html> " | sudo tee /data/web_static/releases/test/index.html
 sudo rm -rf /data/web_static/current
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a \\tlocation /hbnb_static/ {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
+exit(0)

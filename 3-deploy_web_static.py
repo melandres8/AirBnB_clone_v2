18fbd96b1dd262f9c470a8825ee389989d439e4e
melandres8 <melanmosta@gmail.com>
2020-08-18 03:34:11 +0000

    task#3: adjustments

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,15 +5,54 @@ from fabric.api import *
 from datetime import datetime
 
 
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
 env.hosts = ['35.237.26.128', '3.92.66.113']
 path = None
+
+
+def do_pack():
+    """ Generate the archive .tgz """
+    ftime = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    fname = "versions/web_static_{}.tgz".format(ftime)
+
+    if fname:
+        local('sudo mkdir -p ./versions')
+        local('tar -cvzf {} ./web_static'.format(fname))
+        return fname
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """ distributes an archive to the
+        web server """
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
+        arc = archive_path.split('/')[-1]
+        path = '/data/web_static/releases'
+        directory = arc.split('.')
+        put("{}".format(archive_path), "/tmp/{}".format(arc))
+        run("sudo mkdir -p {}/{}/".format(path, directory[0]))
+        run("sudo tar -xzf /tmp/{} -C {}/{}/".format(arc, path, directory[0]))
+        run("sudo rm /tmp/{}".format(arc))
+        run("sudo mv {}/{}/web_static/* {}/{}/\
+            ".format(path, directory[0], path, directory[0]))
+        run("sudo rm -rf {}/{}/web_static".format(path, directory[0]))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -sf {}/{}/ /data/web_static/current\
+            ".format(path, directory[0]))
+        print('New version deployed!')
+        return True
+    except Exception:
+        return False
+
 
 def deploy():
     """ script (based on the file 2-do_deploy_web_static.py) that creates
         and distributes an archive to your web servers,
         using the function deploy """
+    global path
+
     path = do_pack()
     if path is None:
         return False

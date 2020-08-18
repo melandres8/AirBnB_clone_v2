315217ba65fc4845d04b44f4afce4dc593fe8fe6
melandres8 <melanmosta@gmail.com>
2020-08-18 03:29:21 +0000

    task#3: adjustments

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,16 +8,13 @@ from datetime import datetime
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 env.hosts = ['35.237.26.128', '3.92.66.113']
-path = ''
-
+path = None
 
 def deploy():
     """ script (based on the file 2-do_deploy_web_static.py) that creates
         and distributes an archive to your web servers,
         using the function deploy """
-    global path
-    if not path:
-        path = do_pack()
-    if not path:
+    path = do_pack()
+    if path is None:
         return False
     return do_deploy(path)

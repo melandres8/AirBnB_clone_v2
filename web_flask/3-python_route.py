6617040d34c08f49ab3c03cf42c90a76d51d74dd
melandres8 <melanmosta@gmail.com>
2020-08-31 22:47:43 +0000

    task#3: python

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    string = text.replace('_', ' ')
+    return "C {}".format(string)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_cool(text='is cool'):
+    string = text.replace('_', ' ')
+    return "Python {}".format(string)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')

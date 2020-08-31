8ab878f7f951b84d1a701251077a3596b5b189c0
melandres8 <melanmosta@gmail.com>
2020-08-31 23:04:34 +0000

    task#5: rendering templates

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    string = text.replace('_', ' ')
+    return "C {}".format(string)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_cool(text='is cool'):
+    string = text.replace('_', ' ')
+    return "Python {}".format(string)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')

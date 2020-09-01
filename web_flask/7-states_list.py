df072c05a01802b0c10357184d00cb8674bb9e1e
melandres8 <melanmosta@gmail.com>
2020-09-01 03:23:43 +0000

    task#7: list of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Closes the database again at
+        the end of the request. """
+    storage.close()
+
+@app.route('/states_list', strict_slashes=False)
+def states():
+    """ Render state template """
+    list_states = storage.all(State).values()
+    return render_template('7-states_list.html', all_states=list_states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
